80d8ac13ed007f204408099056e90e5c17f7f2fe
agataszus <agataszus@gmail.com>
2023-01-15 17:55:37 +0000

    feat(mobile navigation): added js for mobile navigation and  copyrights

---
+++ js/script.js
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////
+// Set current year
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
